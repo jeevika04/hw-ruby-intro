69515a3fc1be585149b1f841306ef23dbe0ce187
EC2 Default User <ec2-user@ip-172-31-38-14.us-east-2.compute.internal>
2020-01-19 20:05:40 +0000

    Addes and tested Part 1,2,3

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,35 +1,67 @@
 # When done, submit this entire file to the autograder.
-
+#CSCE 431 Spring 2020
+# Jeevika Jarmarwala 
 # Part 1
 
+# Used https://ruby-doc.org/core-2.4.1/Array.html to complete Part 1 
 def sum arr
-  # YOUR CODE HERE
+  sum_array = 0 
+  arr.each { |x| sum_array = sum_array + x } 
+  return sum_array
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  sorted_array = arr.sort
+  reverse_array = sorted_array.reverse
+  sum(reverse_array.take(2))
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
-end
+  array_pairs = arr.combination(2).to_a
+  array_pairs.each do |x|
+    return true if sum(x) == n
+  end
+  return false 
+end 
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  name = "Hello, " + name
+  return name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  /^[^aeiouAEIOU\W]/x.match(s) != nil
 end
 
+# https://stackoverflow.com/questions/32536143/check-if-string-has-only-0-and-1
+
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return true if s == "0"
+  if (/[^01]/ !~ s)
+    return (/^[01]*00$/.match(s) != nil)
+  end
 end
 
 # Part 3
 
+# https://www.rubyguides.com/2012/01/ruby-string-formatting/
+
 class BookInStock
-# YOUR CODE HERE
+  
+  attr_accessor :isbn
+  attr_accessor :price
+  
+  def initialize(isbn, price)
+    raise ArgumentError, 'Arguments are not valid' if isbn.empty? or price <= 0
+    @isbn = isbn
+    @price = price
+  end
+  
+  def price_as_string
+    sprintf("$%2.2f",@price)
+  end
+    
+  
 end
